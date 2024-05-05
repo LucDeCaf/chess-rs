dfb05d6d33ac0fc49a3e0a6819ea3a441c397043
LucDeCaf <ldecafmeyer@gmail.com>
2024-05-05 14:24:34 +0200

    fix: fix white pawn capture generation

--- src/board_helper.rs
+++ src/board_helper.rs
@@ -42,19 +42,21 @@ impl BoardHelper {
     pub fn generate_white_pawn_capture_masks() -> [u64; 64] {
         let mut masks = [0; 64];
         let mut rank: usize;
+        let mut mask: usize;
 
         for i in 8..56 {
+            mask = 0;
             rank = i / 8;
-            
+
             if rank.abs_diff((i + 9) / 8) <= 1 {
-                masks[i] |= (i as u64) << 9;
-            }
-            
-            if rank.abs_diff((i + 7) / 8) <= 1 {
-                masks[i] |= (i as u64) << 7;
+                mask |= 1 << (i + 9);
             }
 
-            masks[i] = masks[i].reverse_bits();
+            if rank.abs_diff((i + 7) / 8) <= 1 {
+                mask |= 1 << (i + 7);
+            }
+
+            masks[i] = mask.reverse_bits() as u64;
         }
 
         masks
@@ -141,72 +143,4 @@ impl BoardHelper {
             println!("{}", line);
         }
     }
-}
-
-#[derive(Debug)]
-pub enum Square {
-    A1,
-    A2,
-    A3,
-    A4,
-    A5,
-    A6,
-    A7,
-    A8,
-    B1,
-    B2,
-    B3,
-    B4,
-    B5,
-    B6,
-    B7,
-    B8,
-    C1,
-    C2,
-    C3,
-    C4,
-    C5,
-    C6,
-    C7,
-    C8,
-    D1,
-    D2,
-    D3,
-    D4,
-    D5,
-    D6,
-    D7,
-    D8,
-    E1,
-    E2,
-    E3,
-    E4,
-    E5,
-    E6,
-    E7,
-    E8,
-    F1,
-    F2,
-    F3,
-    F4,
-    F5,
-    F6,
-    F7,
-    F8,
-    G1,
-    G2,
-    G3,
-    G4,
-    G5,
-    G6,
-    G7,
-    G8,
-    H1,
-    H2,
-    H3,
-    H4,
-    H5,
-    H6,
-    H7,
-    H8,
 }
