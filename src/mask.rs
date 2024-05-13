1963f769f679e187b6a7d32252bc8f4adaf05886
LucDeCaf <ldecafmeyer@gmail.com>
2024-05-13 16:23:32 +0200

    chore: add zeroes of mask

--- src/mask.rs
+++ src/mask.rs
@@ -2,14 +2,32 @@ use std::ops::{BitAnd, BitAndAssign, BitOr, BitOrAssign, BitXor, BitXorAssign, N
 
 use crate::square::Square;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 pub struct Mask(pub u64);
 
 impl Mask {
-    pub fn squares(&self) -> Vec<Square> {
+    pub fn ones(&self) -> Vec<Square> {
         let mut squares = vec![];
 
         let mut square = self.0;
+        let mut i = 0;
+
+        while square > 0 {
+            if square & 1 == 1 {
+                squares.push(Square::from_u8(i).unwrap());
+            }
+
+            square >>= 1;
+            i += 1;
+        }
+
+        squares
+    }
+
+    pub fn zeroes(&self) -> Vec<Square> {
+        let mut squares = vec![];
+
+        let mut square = !self.0;
         let mut i = 0;
 
         while square > 0 {
@@ -82,15 +100,15 @@ mod mask_tests {
     #[test]
     fn test_mask_squares() {
         assert_eq!(
-            Mask(0b00000000000000000000000000000001).squares(),
+            Mask(0b00000000000000000000000000000001).ones(),
             vec![Square::A1]
         );
         assert_eq!(
-            Mask(0b00000000000000000000000100000001).squares(),
+            Mask(0b00000000000000000000000100000001).ones(),
             vec![Square::A1, Square::A2]
         );
         assert_eq!(
-            Mask(0b00000000000000000101000100000001).squares(),
+            Mask(0b00000000000000000101000100000001).ones(),
             vec![Square::A1, Square::A2, Square::E2, Square::G2]
         );
     }
